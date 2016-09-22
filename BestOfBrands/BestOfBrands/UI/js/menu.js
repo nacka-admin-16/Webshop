b3fdda19cecd897dfeb2d71fa48c5fdd63966393
nacka-admin-16 <carl.schele@nackademin.se>
2016-09-22 23:47:03 +0200

    adding menu js

---
+++ BestOfBrands/BestOfBrands/UI/js/menu.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/") + 1);
+
+    $("#navbar ul li a").each(function () {
+        if ($(this).attr("href") === '/Pages/' + pgurl || $(this).attr("href") === '') {
+            $(this).closest('li').addClass("active");
+        } else {
+            $(this).closest('li').removeClass("active");
+        }                        
+    });
+});
