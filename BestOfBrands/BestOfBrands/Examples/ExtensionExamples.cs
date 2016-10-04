3bf4316115b7939be77c06810399fa34715b5f0f
nacka-admin-16 <carl.schele@nackademin.se>
2016-10-04 10:30:14 +0200

    extensionmetoder

---
+++ BestOfBrands/BestOfBrands/Examples/ExtensionExamples.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace BestOfBrands.Examples
+{
+    public static class ExtensionExamples
+    {
+        public static int CountWords(this string myString)
+        {
+            var words = myString.Trim().Split(' ');
+            
+            return words.Length;
+        }
+        
+        public static bool ContainsChar(this string myString, string myChar)
+        {
+            var status = myString.Contains(myChar);
+            
+            return status;           
+        }
+        
+        public static string FirstLetterToUpper(this string myString)
+        {
+            return char.ToUpper(myString[0]) + myString.Substring(1);            
+        }
+
+        public static string FirstLetterInEveryWordToUpper(this string myString)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(myString.ToLower());
+        }
+    }
+}
---
+++ BestOfBrands/BestOfBrands/Examples/TryCatch.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using BestOfBrands.Configuration;
+
+namespace BestOfBrands.Examples
+{
+    public class TryCatch
+    {
+        public int ConvertStringToInt(string myString)
+        {
+            try
+            {
+                return Convert(myString);
+            }
+            catch (Exception exception)
+            {
+                using (StreamWriter streamWriter = new StreamWriter(SiteConstants.PathToExceptionLog(), true))
+                {
+                    streamWriter.WriteLine(DateTime.Now + " " + exception.Message);
+                }
+
+                return -1;                
+            }
+        }
+
+        public int Convert(string myString)
+        {
+            try
+            {
+                var convertedString = int.Parse(myString);
+
+                return convertedString;
+            }
+            finally
+            {
+                using (StreamWriter streamWriter = new StreamWriter(SiteConstants.PathToExceptionLog(), true))
+                {
+                    streamWriter.WriteLine(DateTime.Now + " knas");
+                }                
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//try
+//{
+//    var convertedString = int.Parse(myString);
+
+//    return convertedString;
+//}
+//catch (Exception exception)
+//{
+//    using (StreamWriter streamWriter = new StreamWriter(SiteConstants.PathToExceptionLog()))
+//    {
+//        streamWriter.WriteLine(DateTime.Now + " " + exception.Message);
+//    }
+
+//    return -1;
+//}
